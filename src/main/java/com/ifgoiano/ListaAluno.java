5c436a748469f5a1000e43c3df4c67c53689543c
Jeff <jeffersonpereira79iu@gmail.com>
2026-05-26 09:43:51 -0300

    .

--- src/main/java/com/ifgoiano/ListaAluno.java
+++ src/main/java/com/ifgoiano/ListaAluno.java
@@ -1,4 +1,46 @@
 package com.ifgoiano;
 
 public class ListaAluno {
+    private Nodo head;
+    private Nodo tail;
+
+    public ListaAluno() {
+        this.head = null;
+        this.tail = this.head;
+    }
+
+    public void insercaoEmOrdem(Aluno aluno, double nota) {
+        Nodo novo = new Nodo(aluno.getMatricula(), nota);
+
+        if (this.head == null) {
+            this.head = novo;
+            this.tail = this.head;
+        }
+        else {
+
+        }
+    }
+
+    public void remocaoPorMatricula(Aluno aluno) {
+        Nodo aux  = this.head;
+
+        if (aux == null) {
+            return;
+        }
+        else if (aux.matricula.equals(aluno.getMatricula())) {
+            this.head = aux.prox;
+            if (this.head == null) {
+                this.tail = null;
+            }
+            return;
+        }
+
+        while (aux.prox != null ) {
+            if (aux.prox.matricula.equals(aluno.getMatricula())) {
+                
+                return;
+            }
+            aux = aux.prox;
+        }
+    }
 }
---
+++ src/main/java/com/ifgoiano/Nodo.java
@@ -0,0 +1,12 @@
+package com.ifgoiano;
+
+public class Nodo {
+    String matricula;
+    double nota;
+    Nodo prox;
+
+    public Nodo(String matricula, double nota) {
+        this.matricula = matricula;
+        this.nota = nota;
+    }
+}

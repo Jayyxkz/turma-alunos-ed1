600b60d89e4133c64d23ced345beb5b9e8bbe6c3
Jeff <jeffersonpereira79iu@gmail.com>
2026-05-27 17:31:54 -0300

    buscas

--- src/main/java/com/ifgoiano/TesteListaAluno.java
+++ src/main/java/com/ifgoiano/TesteListaAluno.java
@@ -1,7 +1,47 @@
 package com.ifgoiano;
 
+import com.ifgoiano.models.Aluno;
+import com.ifgoiano.services.ListaNotas;
+
+import java.util.List;
+
 public class TesteListaAluno {
     public static void main(String[] args) {
 
+        ListaNotas lista = getListaNotas();
+
+        Aluno menorDesempenho = lista.buscaPorNota(4.3f);
+        System.out.println("Alunos com menor desempenho:\n" + menorDesempenho.getNome());
+
+        lista.remocaoPorMatricula("15010");
+        System.out.println("\nAluno Jefferson removido!");
+
+        List<Aluno> alunosOrizona = lista.buscaPorCidade("Orizona");
+        System.out.println("\nAlunos de Orizona:");
+        for (Aluno aluno : alunosOrizona) {
+            System.out.println(aluno.getNome());
+        }
+
+        System.out.println("\nTamanho da lista: " + lista.tamanho());
+    }
+
+    // Insere alunos na lista
+    private static ListaNotas getListaNotas() {
+        Aluno aluno1 = new Aluno("15010", "Jefferson", "Sistemas de Informação", "Orizona",
+                "64998888");
+        Aluno aluno2 = new Aluno("15011", "Flávio", "Sistemas de Informação", "Orizona",
+                "64998383");
+        Aluno aluno3 = new Aluno("15012", "Paulo", "Sistemas de Informação", "Orizona",
+                "64994788");
+        Aluno aluno4 = new Aluno("15013", "João", "Sistemas de Informação", "Urutaí",
+                "64994777");
+
+        ListaNotas lista = new ListaNotas();
+
+        lista.insercaoEmOrdem(aluno1, 6.7f);
+        lista.insercaoEmOrdem(aluno2, 7.4f);
+        lista.insercaoEmOrdem(aluno3, 7.5f);
+        lista.insercaoEmOrdem(aluno4, 4.3f);
+        return lista;
     }
 }
---
+++ src/main/java/com/ifgoiano/services/ListaNotas.java
@@ -0,0 +1,121 @@
+package com.ifgoiano.services;
+
+import com.ifgoiano.models.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaNotas {
+
+    private static class Nodo {
+        Aluno aluno;
+        float nota;
+        Nodo prox;
+
+        public Nodo(Aluno aluno, float nota) {
+            this.aluno = aluno;
+            this.nota = nota;
+        }
+    }
+
+    private Nodo head;
+    private Nodo ultimo;
+
+    public ListaNotas() {
+        head = new Nodo(null, 0.0f);
+        ultimo = head;
+    }
+
+    public void insercaoEmOrdem(Aluno aluno, float nota) {
+        Nodo novo = new Nodo(aluno, nota);
+        Nodo aux = this.head;
+
+        while (aux.prox != null && aux.prox.nota >= nota) {
+            aux = aux.prox;
+        }
+
+        novo.prox = aux.prox;
+        aux.prox = novo;
+
+        if (novo.prox == null) {
+            this.ultimo = novo;
+        }
+    }
+
+    public void remocaoPorMatricula(String matricula) {
+        Nodo aux  = this.head;
+
+        while (aux.prox != null && !aux.prox.aluno.getMatricula().equals(matricula)) {
+            aux = aux.prox;
+        }
+
+        if (aux.prox == null) {
+            return;
+        }
+
+        Nodo q = aux.prox;
+        aux.prox = q.prox;
+
+        if (aux.prox == null) {
+            this.ultimo = aux;
+        }
+    }
+
+    public Aluno buscaPorNota(float nota) {
+        Nodo aux = this.head.prox;
+
+        while (aux != null) {
+            if (aux.nota == nota) {
+                return aux.aluno;
+            }
+            aux = aux.prox;
+        }
+        System.out.println("Aluno não encontrado!");
+        return null;
+    }
+
+    public Aluno buscaPorMatricula(Aluno aluno) {
+        Nodo aux = this.head.prox;
+        String target = aluno.getMatricula();
+
+        while (aux != null) {
+            if (aux.aluno.getMatricula().equals(target)) {
+                return aux.aluno;
+            }
+            aux = aux.prox;
+        }
+        System.out.println("Aluno não encontrado!");
+        return null;
+    }
+
+    public List<Aluno> buscaPorCidade(String cidade) {
+        Nodo aux = this.head.prox;
+        List<Aluno> lista = new ArrayList<>();
+
+        while (aux != null) {
+            if (aux.aluno.getEndereco().equals(cidade)) {
+                lista.add(aux.aluno);
+            }
+            aux = aux.prox;
+        }
+
+        if (lista.isEmpty()) {
+            System.out.println("Aluno(s) não encontrado(s)!");
+            return null;
+        }
+        else {
+            return lista;
+        }
+    }
+
+    public int tamanho() {
+        Nodo aux = this.head;
+        int tamanho = 0;
+
+        while (aux.prox != null) {
+            tamanho++;
+            aux = aux.prox;
+        }
+        return tamanho;
+    }
+}
